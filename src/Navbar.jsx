faca9e06db1da2434c0623eab895e64c7230d110
Ken <kendo302003@gmail.com>
2022-12-07 08:23:23 +0100

    .js to .jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,6 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Dojo Blog</h1>
-      
       <div className="links">
         <a href="/">Homepage</a>
         <a href="/create">New Blog</a>
