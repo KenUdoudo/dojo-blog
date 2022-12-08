4dff34f4b5bba53597e2aac3ce930c96f01d1c64
Ken <kendo302003@gmail.com>
2022-12-08 14:21:17 +0100

    filtering list

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,8 @@ const [blogs, setBlogs] = useState([
 
   return (
     <div className="home">
-      <BlogList blogs={blogs}/>
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
     </div>
   );
 }
